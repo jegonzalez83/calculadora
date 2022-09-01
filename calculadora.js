521b93d5e1a6c2167def19b9f6dfa03459453dbd
Jhon González <enriquemime@gmail.com>
2022-09-01 11:56:32 -0300

    creacion repo en git hub

---
+++ calculadora.js
@@ -0,0 +1,13 @@
+let suma = require("./sumar")
+let resta = require("./restar")
+let multiplicacion = require("./multiplicar")
+let division = require("./dividir")
+
+
+console.log(suma(1,2))
+console.log(resta(2,1))
+console.log(multiplicacion(2,1))
+console.log(multiplicacion(2,0))
+console.log(division(4,2))
+console.log(division(2,0))
+console.log(division(0,2))
